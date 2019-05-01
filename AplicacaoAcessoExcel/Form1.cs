8ea617f22c73fe2b2d5c3413a4f877ab49c367f5
professoralessandro <professor_alessandro@hotmail.com>
2019-05-01 15:27:05 -0300

    first commit

---
+++ AplicacaoAcessoExcel/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// USANDO
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Tools.Excel;
+//USANDO
+namespace AplicacaoAcessoExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Worksheet.activeWorksheet = Globals.ThisAddIn.Application.ActiveSheet;
+        }
+    }//CLASS FORM
+}
---
+++ AplicacaoAcessoExcel/Ribbonar_Teste.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//usar
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Tools.Excel;
+//usar
+
+namespace AplicacaoAcessoExcel
+{
+    class Ribbonar_Teste
+    {
+    }
+}
